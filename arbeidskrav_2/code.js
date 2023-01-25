0da337e24c3a0fcab43878b535472522c20cea6d
Mariannesm <marmyr6@hotmail.com>
2023-01-25 18:55:35 +0100

    Endringer
    gjorde om javascript.
    samme med css for å få den responsiv.

--- arbeidskrav_2/code.js
+++ arbeidskrav_2/code.js
@@ -2,7 +2,7 @@
 let meny =""
 
 resources.map((menyItem, index) => {
-    meny += `<a onclick="tab('${index}')" class="${index == 0 ? 'active' : ""}">${menyItem.category}</a>`
+    meny += `<a id="fane-knapp-${index}" onclick="tab('${index}')">${menyItem.category}</a>`
     }) 
 
 document.querySelector("#faneMeny").innerHTML = meny
@@ -11,10 +11,21 @@ document.querySelector("#faneMeny").innerHTML = meny
 
 
 // Håndtere tabs lage onclick 
-
+//legge inn alle elementene i nav mister klassen active klassen, slik at ingen har den klassen her? 
 function tab(index) {
+    // Henter objektet basert på indeks
      menyItem = resources[index]
      console.log(menyItem)
+
+     // Fjerner active klassen fra elementet med klassen active
+     let buttonActive = document.querySelector(".active")
+
+     // Hvis ikke knappen finnes, så skal ikke .active fjernes fra noen knapp
+     if(buttonActive != null) {
+        buttonActive.classList.remove("active")
+     }
+
+     document.querySelector("#fane-knapp-" + index).classList.add("active")
 
      document.querySelector("#title").innerHTML = menyItem.category
      document.querySelector("#content").innerHTML = menyItem.text
@@ -26,13 +37,14 @@ function tab(index) {
         
      })
 
-     document.querySelector("#links").innerHTML = linker
-
-     
+     document.querySelector("#links").innerHTML = linker  
 
 }
 
-//alle elementene i nav mister klassen active klassen, slik at ingen har den klassen
+
+// Henter ut første index i ressurser (HTML)
+tab(0)
+
 
 
 
